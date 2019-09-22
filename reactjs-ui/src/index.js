e591f6b291d5f0f94a83b563cda3ee4f0a6f0910
Achim Geisler <project_design@web.de>
2019-09-22 21:12:25 +0200

    added a modal and general css

--- reactjs-ui/src/index.js
+++ reactjs-ui/src/index.js
@@ -2,6 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function PersonDescModal(props) {
+  return (
+    <div className="sample-plugin-person-modal" style={{display: props.display}} onClick={props.onClick}>
+      <div className="person-modal-frame">
+        <div className="person-modal-header">
+          <h3 className="modal-header-headline">{props.position}</h3>
+          <span className="modal-header-close" onClick={props.onClick}>&times;</span>
+        </div>
+        <div className="modal-content">
+          <p>{props.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 
 function PersonPic(props) {
   return (
@@ -32,29 +48,48 @@ class Person extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      modalDisplay: "none",
       firstName: "John",
       lastName: "Doe",
       position: "CEO",
-      picture: null
+      description: "CEO is a chief executive officer, the highest-rankig corporate officer (executive) or administrator in an organisation.",
+      picture: null,
 
+    }
+  }
+
+  handleClick() {
+    if (this.state.modalDisplay == "none") {
+      this.setState({modalDisplay: "block"});
+    }
+    else {
+      this.setState({modalDisplay: "none"});
     }
   }
 
   render() {
     return (
       <div className="sample-plugin-person">
-        <PersonPic
-          picture={this.state.picture}
-        />
-        <div className="sample-plugin-person-detail">
-          <PersonName
-            firstName={this.state.firstName}
-            lastName={this.state.lastName}
+          <div className="sample-plugin-person-card" onClick={() => this.handleClick()}>
+          <PersonPic
+            picture={this.state.picture}
           />
-          <PersonPos
-            position={this.state.position}
-          />
+          <div className="sample-plugin-person-detail">
+            <PersonName
+              firstName={this.state.firstName}
+              lastName={this.state.lastName}
+            />
+            <PersonPos
+              position={this.state.position}
+            />
+          </div>
         </div>
+        <PersonDescModal
+          display={this.state.modalDisplay}
+          onClick={() => this.handleClick()}
+          position={this.state.position}
+          description={this.state.description}
+        />
       </div>
     );
   }
