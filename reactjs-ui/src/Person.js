f692c82e6b1be295e3fd473d8bc1751b80e7dd86
Achim Geisler <project_design@web.de>
2019-09-28 13:16:53 +0200

    complete refactoring of reactjs code base

---
+++ reactjs-ui/src/Person.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PersonDescrModal from './PersonDescrModal.js';
+import './Person.css';
+import data from './sample_person_data.json';
+
+const persons = data.persons;
+let person = "";
+
+function PersonPic(props) {
+  let altText = "Picture of " + props.image;
+  return (
+    <div className="sample-plugin-person-pic">
+      <img src={props.image} alt={altText} height="400" width="400"/>
+    </div>
+  );
+}
+
+function PersonName(props) {
+  return (
+    <div className="sample-plugin-person-fname">
+      {props.firstLastName == null ? "Name not recognized!" : props.firstLastName}
+    </div>
+  );
+}
+
+function PersonPos(props) {
+  return (
+    <div className="sample-plugin-person-pos">
+      {props.position}
+    </div>
+  );
+}
+
+
+class Person extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalDisplay: "none"
+    }
+
+    for (let p of persons) {
+      if (p.firstLastName.toLowerCase() === this.props.person_name.toLowerCase()) {
+        person = p;
+        break;
+      }
+    }
+  }
+
+  handleClick() {
+    if (this.state.modalDisplay === "none") {
+      this.setState({modalDisplay: "block"});
+    }
+    else {
+      this.setState({modalDisplay: "none"});
+    }
+  }
+
+  render() {
+    return (
+      <div className="sample-plugin-person">
+          <div className="sample-plugin-person-card" onClick={() => this.handleClick()}>
+          <PersonPic
+            image={null}
+          />
+          <div className="sample-plugin-person-detail">
+            <PersonName
+              firstLastName={person.firstLastName}
+            />
+            <PersonPos
+              position={person.position}
+            />
+          </div>
+        </div>
+        <PersonDescrModal
+          display={this.state.modalDisplay}
+          onClick={() => this.handleClick()}
+          position={person.position}
+          description={person.description}
+          social={person.social}
+        />
+      </div>
+    );
+  }
+}
+
+export default Person;
---
+++ reactjs-ui/src/PersonDescrModal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './PersonDescrModal.css';
+
+function PersonDescrModal(props) {
+  return (
+    <div className="sample-plugin-person-modal" style={{display: props.display}} onClick={props.onClick}>
+      <div className="person-modal-frame">
+        <div className="person-modal-header">
+          <h3 className="modal-header-headline">{props.position}</h3>
+          <span className="modal-header-close" onClick={props.onClick}>&times;</span>
+        </div>
+        <div className="modal-content">
+          <p>{props.description}</p>
+        </div>
+        <div className="modal-social">
+          <ul className="modal-social-list">
+            <li><a href={props.social ? props.social.github : ""}>Github</a></li>
+            <li><a href={props.social ? props.social.linkedin : ""}>LinkedIn</a></li>
+            <li><a href={props.social ? props.social.xing : ""}>Xing</a></li>
+            <li><a href={props.social ? props.social.facebook : ""}>Facebook</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PersonDescrModal;
