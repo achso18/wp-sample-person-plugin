ce3e5e5615c5d92b38bd8343c50b215944c21dc1
Achim Geisler <project_design@web.de>
2019-09-28 13:23:01 +0200

    minor updates

--- reactjs-ui/src/index.js
+++ reactjs-ui/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Person from './Person.js';
 import './index.css';
-import data from './sample_person_data.json';
 
-let rootElement = document.getElementById('root');
+let rootElement = document.getElementById('wpsp-person-root');
 ReactDOM.render(<Person person_name={rootElement.getAttribute('person_name')}/>, rootElement);
